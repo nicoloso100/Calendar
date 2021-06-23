0a2a73449efec829bc24f14b1303aed494351092
Nicolás Angarita Ortiz <nicolas.angarita@masivian.com>
2021-06-23 00:08:18 -0500

    Controllers configuration

---
+++ Calendar/CalendarApp/Controllers/GeneralController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalendarApp.Controllers
+{
+    public class GeneralController : ControllerBase
+    {
+        public GeneralController()
+        {
+
+        }
+    }
+}
