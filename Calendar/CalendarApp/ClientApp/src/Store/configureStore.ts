43271fe4ae042f0002e898d065e8faf9b6bab1a5
Nicolás Angarita Ortiz <nicolas.angarita@masivian.com>
2021-06-22 21:38:57 -0500

    project initialization

---
+++ Calendar/CalendarApp/ClientApp/src/Store/configureStore.ts
@@ -0,0 +1,4 @@
+import { applyMiddleware, createStore } from "redux";
+import createSagaMiddleware from "redux-saga";
+
+const sagaMiddleware = createSagaMiddleware();
---
+++ Calendar/CalendarServices/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CalendarServices
+{
+    public class Class1
+    {
+    }
+}
